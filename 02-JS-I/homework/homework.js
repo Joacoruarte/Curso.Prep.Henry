cb16053fd5776f42e55e1ff37e604fccab0df9d6
Joacoruarte <ruartejoaquin@gmail.com>
2021-11-04 21:15:29 -0300

    finish JS1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -208,28 +208,23 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código: 
-  var str =  "hello world"+"!"; 
- 
-  return str;
+  
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código: 
-  var combinarNombre= (nombre+"  "+apellido); 
-  return combinarNombre;
+  return nombre+" "+apellido;
 }
-combinarNombres('Joaquin' , 'Ruarte'); 
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var nombre = "Martin";
-  var saludo = "Hola"; 
-
-  return nombre+saludo;
+ 
+  return "Hola "+ nombre+"!";
 } 
  
 
@@ -237,21 +232,21 @@ function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:  
 
-  
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  return (base * altura) / 2;
 }
 
 
@@ -281,12 +276,8 @@ function esVocal(letra){
   }else{ 
     return "Dato incorrecto";
   }
-  
- 
 
-
-}
-
+}  
 
 
 // No modificar nada debajo de esta línea
