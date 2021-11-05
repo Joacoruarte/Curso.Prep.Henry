f10306997060a565cf8bb8f6c599d2f40c60eb17
Joacoruarte <ruartejoaquin@gmail.com>
2021-11-05 14:40:15 -0300

    Commits

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -101,29 +101,68 @@ function agregarNumeros(numeros) {
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-  // Tu código:
+  // Tu código: 
+ var suma = 0 
+ for(var i = 0; i < resultadosTest.length ; i++){ 
+  suma = suma + resultadosTest[i]; 
+}  
+
+var promedio = suma / resultadosTest.length; 
+
+ return promedio;
 }
 
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
-  // Tu código:
+  // Tu código:  
+  var mayor = numeros[0]; 
+
+  for(var i = 0 ; i < numeros.length; i++){ 
+    if(numeros[i] > mayor){ 
+      mayor = numeros[i];
+    }
+  } 
+ 
+  return mayor;
+
 }
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
-  // Escribe tu código aquí:
-}
+  // Escribe tu código aquí: 
+  if(arguments.length < 1){ 
+    return 0; 
+  } 
+
+  var producto = 1;  
+  
+  for(var i = 0; i < arguments.length; i++){ 
+    producto = producto * arguments[i];
+  } 
+  return producto;
+} 
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
-  //Escribe tu código aquí
+  //Escribe tu código aquí 
 
-}
+  var mayorque = 0;
+
+  for(var i = 0 ; i < arreglo.length ;i++){ 
+      if(arreglo[i] > 18){ 
+        mayorque++;
+      }
+    } 
+    return mayorque;
+  } 
+  
+  
+
 
 
 function diaDeLaSemana(numeroDeDia) {
@@ -131,14 +170,28 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
-} 
+ 
+  if( numeroDeDia === 1 || numeroDeDia === 7){ 
+    return "Es fin de semana";
+  }else{ 
+    return "Es dia Laboral";
+  }
+ 
+}
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
-  //Escribe tu código aquí
+  //Escribe tu código aquí 
+  
+  let num = n.toString();
+  
+  if(num.charAt(0) === "9"){ 
+    return true;
+  }else{ 
+    return false;
+  }
   
 }
 
@@ -146,8 +199,13 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
+  //Escribe tu código aquí   
+  for (var i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
+    }
+  }
+  return true
 } 
 
 
@@ -155,8 +213,23 @@ function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
-  // Tu código:
+  // Tu código: 
+ 
+
+  var nuevoArray = [];
+  for(let i= 0; i<array.length; i++) {
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      nuevoArray.push(array[i]);
+    }
+  }
+  if(nuevoArray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  }
+  else {
+      return nuevoArray;
+  }
 }
+
 
 
 function mayorACien(array) {
