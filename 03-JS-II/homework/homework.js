dae978c47e862c1e66cb6b841359ca3ff029a5d0
Joacoruarte <ruartejoaquin@gmail.com>
2021-11-29 10:57:49 -0300

    a

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -236,6 +236,9 @@ function doWhile(numero) {
  return numero;
 }
 
+Array.push(); "Este metodo me agrega al final de una array un elemento nuevo" 
+Array.unshift(); "Este metodo me agrega al principio de una array un elemento nuevo" 
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
